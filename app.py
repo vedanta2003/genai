ff745805dbeed32f4b77f72df10b1c559208785f
Vedanta Yadav <yadavvedanta19@gmail.com>
2024-02-15 12:53:55 +0530

    commit

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-from langchain.llms import huggingface_hub
+from langchain.llms import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import streamlit as st
+import os
+# function to load huggingface model
+
+def get_responses(questions):
+    llm=openai(openai_api_key=os.environ['OPEN_API_KEY'],model_name="text-davinci-003")
